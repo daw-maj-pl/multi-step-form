21cb9919157836485cfd4c0c2b43b4c7bebdc5f7
daw-maj-pl <71944368+daw-maj-pl@users.noreply.github.com>
2021-10-15 14:01:55 +0200

    Create FormUserDetails component

---
+++ src/components/FormUserDetails.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+
+export class FormUserDetails extends Component {
+  render() {
+    return <div>FormUserDetails</div>;
+  }
+}
+
+export default FormUserDetails;
